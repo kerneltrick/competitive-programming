eed598c0f52b9964e2a67fd01be565e29f2c4995
Mark <markjimenez3@gmail.com>
2021-02-18 22:17:41 -1000

    fleaonachessboard failed submission timed out

---
+++ test_bench/fleaonachessboard.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <iostream>
+
+#include <math.h>
+
+
+using namespace std;
+
+
+int is_white_tile(int sideLength, int x, int y){
+	int x_tile = x/sideLength;
+       	int y_tile = y/sideLength;	
+	if ((x_tile + y_tile)%2 == 0)
+		return false;
+	if(x%sideLength == 0 || y%sideLength == 0)
+		return false;
+
+	return true;
+}
+
+bool cannot_escape(int sideLength, int x, int y, int dx, int dy){
+	if (is_white_tile(sideLength, x, y))
+		return false;
+	if (dx == 1 && sideLength != 1)
+		return false;
+	if (dy == 1 && sideLength != 1)
+		return false;
+	if (dx % sideLength != 0 && sideLength % dx != 0)
+		return false;
+	if (dy % sideLength != 0 && sideLength % dy != 0)
+	       return false;	
+	return true;
+}
+
+int main(){
+	
+	int sideLength=0;
+	int x=0;
+	int y=0;
+	int dx=0;
+	int dy=0;
+
+	cin >> sideLength >> x >> y >> dx >> dy;
+	while(sideLength != 0 || x != 0 || y!=0 || dx != 0 || dy!= 0 ){
+		if(cannot_escape(sideLength, x, y, dx, dy))
+			cout << "The flea cannot escape from black squares.\n";
+		else{
+			int jumps = 0;
+			
+			while(not is_white_tile(sideLength, x, y)){
+				jumps++;
+				x += dx;
+				y += dy;
+			}
+			printf("After %d jumps the flea lands at (%d, %d).\n", jumps, x, y);
+		}
+		cin >> sideLength >> x >> y >> dx >> dy;
+	}
+	
+	return 0;
+}
