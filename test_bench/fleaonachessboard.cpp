5bc1b0548b9e652e16fa6290a8e3ee307ace7e38
Mark <markjimenez3@gmail.com>
2021-02-19 00:19:28 -1000

    submitted with wrong answer error

--- test_bench/fleaonachessboard.cpp
+++ test_bench/fleaonachessboard.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-
+//checks tile color
 int is_white_tile(int sideLength, int x, int y){
 	int x_tile = x/sideLength;
        	int y_tile = y/sideLength;	
@@ -18,16 +18,6 @@ int is_white_tile(int sideLength, int x, int y){
 	return true;
 }
 
-bool cannot_escape(int sideLength, int x, int y, int dx, int dy){
-	if (is_white_tile(sideLength, x, y))
-		return false;
-	if (dx % sideLength != 0 && sideLength % dx != 0)
-		return false;
-	if (dy % sideLength != 0 && sideLength % dy != 0)
-	       return false;	
-	return true;
-}
-
 int main(){
 	
 	int sideLength=0;
@@ -35,21 +25,36 @@ int main(){
 	int y=0;
 	int dx=0;
 	int dy=0;
+	int start_x=0;
+	int start_y=0;
 
 	cin >> sideLength >> x >> y >> dx >> dy;
+	//End program with line of zeros as input
 	while((sideLength + x + y + dx  + dy) != 0 ){
-		if(cannot_escape(sideLength, x, y, dx, dy))
-			cout << "The flea cannot escape from black squares.\n";
-		else{
-			int jumps = 0;
-			
-			while(not is_white_tile(sideLength, x, y)){
-				jumps++;
-				x += dx;
-				y += dy;
+		start_x = x%sideLength;
+		start_y = y%sideLength;
+		int jumps = 0;
+		while(true){
+			//Check if flea can only land on black squares
+			//If the flea ever lands on at the same mod coordinate pair as it started on,
+			//we know the flea is in a never ending loop
+			if(jumps!=0 and x%sideLength == start_x and y%sideLength == start_y){
+				cout << "The flea cannot escape from black squares.\n";
+				break;
 			}
-			printf("After %d jumps the flea lands at (%d, %d).\n", jumps, x, y);
+			//Keep jumping until flea lands on white tile 
+			if(is_white_tile(sideLength, x, y)){
+				printf("After %d jumps the flea lands at (%d, %d).\n", jumps, x, y);
+				break;
+			}
+			//Count number of iterations through while(flea jumps)
+			jumps++;
+			//Move coordinates
+			x+=dx;
+			y+=dy;
+
 		}
+		//Get new line of input and start again
 		cin >> sideLength >> x >> y >> dx >> dy;
 	}
 	
