c32f7be6a9bef6afd399ce3bd6f7a49957b98397
Mark <markjimenez3@gmail.com>
2021-02-19 00:29:00 -1000

    fixed orderring of checks in flea on a chessboard

--- test_bench/fleaonachessboard.cpp
+++ test_bench/fleaonachessboard.cpp
@@ -35,16 +35,16 @@ int main(){
 		start_y = y%sideLength;
 		int jumps = 0;
 		while(true){
+			//Keep jumping until flea lands on white tile 
+			if(is_white_tile(sideLength, x, y)){
+				printf("After %d jumps the flea lands at (%d, %d).\n", jumps, x, y);
+				break;
+			}
 			//Check if flea can only land on black squares
 			//If the flea ever lands on at the same mod coordinate pair as it started on,
 			//we know the flea is in a never ending loop
 			if(jumps!=0 and x%sideLength == start_x and y%sideLength == start_y){
 				cout << "The flea cannot escape from black squares.\n";
-				break;
-			}
-			//Keep jumping until flea lands on white tile 
-			if(is_white_tile(sideLength, x, y)){
-				printf("After %d jumps the flea lands at (%d, %d).\n", jumps, x, y);
 				break;
 			}
 			//Count number of iterations through while(flea jumps)
