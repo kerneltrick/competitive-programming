7d80e7b05932e0fa599b9ef0e33022ad581c221b
Mark <markjimenez3@gmail.com>
2021-02-19 22:19:14 -1000

    added documentation to rings

--- rings.cpp
+++ rings.cpp
@@ -38,27 +38,32 @@ int main(){
 	while(n != -1){
 		//list of coordinate radius triplets representing rings
 		double rings[n][3]= {0.0};
+
 		//input loop
 		for(int i=0; i < n; ++i){
 			//x, y, radius
 			cin >> rings[i][0] >> rings[i][1] >> rings[i][2];
 
 		}
+		//For DFS, we maintain an adjacency list
 		vector<int> vertices [n];
 		//overlapping graphs are connected
 		for(int i = 0; i < (n-1); ++i){
 			for(int j = (i+1); j < n; ++j){
-				int stuck= overlap(rings[i][0], rings[i][1], rings[i][2], rings[j][0], rings[j][1], rings[j][2]);
+				int stuck = overlap(rings[i][0], rings[i][1], rings[i][2], rings[j][0], rings[j][1], rings[j][2]);
 				if(stuck == 1){
 					vertices[i].push_back(j);
 					vertices[j].push_back(i);
 				}
 			}
 		}
+
 		//find largest connected component
 		int largestComponent = 1;
 		int candidate = 1;
 
+		//DFS also requires a boolean array keeping track of which
+		//nodes have been visited during the search
 		bool visited [n] = { 0 };
 		for(int i = 0; i < n; ++i){
 			candidate = dfs(vertices, visited, i);
