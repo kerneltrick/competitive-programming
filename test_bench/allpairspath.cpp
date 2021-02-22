0e0881e3fedc8da18d125daf0d3320e3c18ba127
Mark <markjimenez3@gmail.com>
2021-02-22 00:42:06 -1000

    added gitignore

---
+++ test_bench/allpairspath.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <climits>
+#define INF INT_MAX
+
+int main(){
+	int n, m, numQueries;
+	scanf("%d %d %d", &n, &m, &numQueries);
+	while((n + m + numQueries) != 0){
+
+
+		scanf("%d %d %d", &n, &m, &numQueries);
+		
+	}
+
+
+}
+
