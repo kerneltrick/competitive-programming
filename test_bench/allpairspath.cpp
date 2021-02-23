b0df9119e0a7b19f8958c95db1538aa4231cd8e7
Mark <markjimenez3@gmail.com>
2021-02-23 00:18:03 -1000

    implemented floyd-warshall

--- test_bench/allpairspath.cpp
+++ test_bench/allpairspath.cpp
@@ -4,28 +4,61 @@
 #include <tuple>
 #include <vector>
 #define INF 100000000 
-typedef std::tuple<int, int, int> triplet;
+typedef std::pair<int, int> triplet;
+typedef std::pair<int, int> PI;
 
-int bellman_ford(std::vector<triplet> edgeList, int distance[], int n, int origin, int target){
+int bellman_ford(std::vector<triplet> edgeList, int n, int origin, int target){
+	int distance[n]; 
+	for (int i = 0; i < n; ++i)
+		distance[i] = INF;
 	distance[origin] = 0;
 
 	for(int i = 0; i < n-1; ++i){
 		for (auto e: edgeList){
 			int a, b, w;
-			std::tie(a, b, w) = e;
-			distance[b] = std::min(distance[b], distance[a] + w);	
-		}	
+			std::tie(a, b, w) = e;				
+			distance[b] = std::min(distance[b], distance[a] + w);					 	           			}   
 	}
-	
+			    
 	for (auto e: edgeList){
 		int a, b, w;
 		std::tie(a,b,w) = e;
 		if(distance[b] > (distance[a] + w ))
 			return -1;
 	}
-
-	return distance[target];
+		return distance[target];
+		
 }
+
+void floyd_warshall(int n, int **adj, int **distance, int origin, int target){
+
+	for (int i = 0; i < n ; ++i)
+	{
+		for (int j=0; j < n; ++j){
+			if(i == j)
+				distance[i][j] = 0;
+			else if (adj[i][j])
+				distance[i][j] = adj[i][j];
+			else
+				distance[i][j] = INF;
+		}
+	}	
+
+	for (int k = 0; k < n; ++k){
+
+		for(int i = 0; i < n; ++i){
+
+			for(int j = 0; j < n; ++j){
+				distance[i][j] = std::min(distance[i][j], (distance[i][k]  + distance[k][j]));
+
+			}
+			//printf("%d " , distance[k][i]);	
+		}
+		//printf("\n");
+
+	}
+
+}	
 
 int main(){
 	int n=0;
@@ -34,38 +67,46 @@ int main(){
 	scanf("%d %d %d", &n, &m, &numQueries);
 	while((n + m + numQueries) != 0){
 
-		std::vector<triplet> edgeList;
+		int *adj [n];
+		for(int i = 0; i < n; i ++)
+			adj[i] = new int[n];
 		for(int i = 0; i < m; ++i){
-			triplet edge;
 			int a, b, w;
 			scanf("%d %d %d", &a, &b, &w);
-			edgeList.push_back({a,b,w});
+			adj[a][b] = w;
 		}
 
 			
 		int origin=0;
 		int target=0;
-		int dist=0;
-		int distance[n]; 
+		int pathDist=0;
+		int *distance [n] ; 
+		for(int i = 0; i < n; ++i)
+			distance[i] = new int[n];
+		floyd_warshall(n, adj, distance, origin, target);
+
+		bool negativeCycle = false;
 		for (int i = 0; i < n; ++i)
-			distance[i] = INF;
+			if(distance[i][i] < 0)
+				negativeCycle = true;
 
 		for(int i = 0; i < numQueries; ++i){
 			scanf("%d %d", &origin, &target );
-			dist = bellman_ford(edgeList, distance, n, origin, target);
-			if(dist == INF)
-				printf("Impossible\n");
-			else if(dist == -1)
+			
+			pathDist = distance[origin][target];
+
+			if(negativeCycle)
 				printf("-Infinity\n");
+			else if(pathDist == INF)
+				printf("Impossible\n");
 			else
-				printf("%d\n", dist);
+				printf("%d\n", pathDist);
 			
 		}	
 		printf("\n");
 		scanf("%d %d %d", &n, &m, &numQueries);
 	}
 	return 0;
-
 
 }
 
