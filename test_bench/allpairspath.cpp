13b0bcaad3d2e860cea92f8511a577d7e5af0d23
Mark <markjimenez3@gmail.com>
2021-02-22 21:21:44 -1000

    building out all pairs

--- test_bench/allpairspath.cpp
+++ test_bench/allpairspath.cpp
@@ -3,14 +3,31 @@
 #include <climits>
 #define INF INT_MAX
 
+int bellman_ford(){
+
+	return 0;
+}
+
 int main(){
-	int n, m, numQueries;
+	int n=0;
+	int m=0;
+       	int numQueries=0;
 	scanf("%d %d %d", &n, &m, &numQueries);
 	while((n + m + numQueries) != 0){
 
-
+		std::vector<int> edgeList;
 		scanf("%d %d %d", &n, &m, &numQueries);
-		
+		for(int i = 0; i < m; ++i){
+			std::tuple<int, int, int> edge;
+			int a, b, w;
+			scanf("%d %d %d", &a, &b, &c);
+			edgeList.push({a,b,w});
+		}
+		int a=0;
+		int b=0;
+		for(int i = 0; i < numQueries; ++i){
+			scanf("%d %d", &a, &b );
+		}	
 	}
 
 
