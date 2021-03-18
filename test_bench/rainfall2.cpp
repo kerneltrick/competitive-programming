18dad8a248ec77c310105aadc3223672fce83cc3
Mark <markjimenez3@gmail.com>
2021-03-17 22:20:18 -1000

    very close on rainfall2, still same error in answer.

--- test_bench/rainfall2.cpp
+++ test_bench/rainfall2.cpp
@@ -7,15 +7,17 @@ int main(){
 	
 	double l=0, k=0, t1=0, t2=0, h=0, f1=0, f2=0;
 	cin >> l >> k >> t1 >> t2 >> h;
-	f1=h;
+
 	if(h<l){
+		f1=h;
 		f2 = h;
 	}else{
+		f1 = h;
 		double max = h + (t2*k);
-		double r = (max / (2*t1)) + (k/2);
-		f2 = r * t1;
+		double r = (max/t1) + ((1-(l/max))*k);
+		f2 = t1 * r;
 	}
 
-	printf("%.6f %.6f", f1, f2);
+	printf("%.6f %.6f\n", f1, f2);
 
 }
