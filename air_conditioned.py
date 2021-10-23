a824e04eff686f7a8d091152bbdd0f55d2a063cb
Mark <markjimenez3@gmail.com>
2021-10-23 11:14:00 -1000

    accepted air conditioned

--- air_conditioned.py
+++ air_conditioned.py
@@ -1,5 +1,6 @@
 from functools import cmp_to_key
 
+# Comparison function for list ordering
 def order(left, right):
 
     L1, U1 = left
@@ -9,26 +10,29 @@ def order(left, right):
         return L1 - L2
     return U1 - U2
 
-
+# Read number of minions
 numMinions = int(input())
 
+# Read in ranges
 minionTempRanges = []
-
 for i in range(numMinions):
 
     tempRange = tuple([int(x) for x in input().split(" ")])
     L, U = tempRange
     minionTempRanges.append(tempRange)
 
-numRooms = 1
-
+# Sort temp ranges
 minionTempRanges.sort(key = cmp_to_key(order))
 roomTemp = minionTempRanges[0][-1]
+print(minionTempRanges)
 
+# Count rooms needed
+numRooms = 1
 for L, U in minionTempRanges:
 
     if roomTemp < L:
         roomTemp = U
         numRooms += 1
 
+# Output
 print(numRooms)
